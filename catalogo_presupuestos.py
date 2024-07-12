82e3b8f5df88be0d517aac72d7a4e010e9ab6adf
Charly_Omar <47049910+CharlyOmar@users.noreply.github.com>
2024-07-12 13:58:47 -0600

    Update catalogo_presupuestos.py
    Se añadió la sección para obtener el total de los padrones seleccionados mediante un filtro.

--- catalogo_presupuestos.py
+++ catalogo_presupuestos.py
@@ -3,20 +3,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #   Declarar el titulo de la aplicación
-st.title("Streamlit App")
+st.set_page_config(page_title='Streamlit App', page_icon='📈', layout='wide')
+
+#Con la siguiente linea se pueden leer todas las hojas del archivo de Excel pero el tiempo de ejecución aumenta considerablemente
+#df = pd.read_excel('D:\Escritorio\Software\Streamlit\Analisis-de-Archivo.xlsx', sheet_name='Catalogo de Presupuestos')
 
 
-df = pd.read_csv('Catalogo_presupuestos.csv', encoding='utf-8', low_memory=False)
+df = pd.read_csv('D:\Escritorio\Software\Streamlit\Catalogo_presupuestos.csv')
 st.caption(r"A continuación se muestra la tabla \"Catálogo de presupuestos\"")
-st.write(df)
+st.dataframe(df)
 
+st.header("Total (resultados filtrados)")
+
+#st.sidebar.header("Filtrar resultados")
+padron_selection = st.multiselect("Selecciona un padrón:", options=df['nombrePadron'].unique(), placeholder="Escoge una opción")
+
+df_selection = df.query("nombrePadron == @padron_selection")
+total_filter = int(df_selection['serie'].sum())
+
+st.caption(f"El total de los resultados obtenidos mediante el uso de los filtros es de:$ :blue[ _{total_filter}_]  MXN")
 #   Crear el gráfico de barras
 counts = df['nombrePadron'].value_counts()
 
 st.header("Padrones")
 st.caption(r"Se muestra una grafica de barras para facilitar la lectura de la frecuencia de los padrones")
 
-fig, ax = plt.subplots(figsize=(8, 3))
+fig, ax = plt.subplots(figsize=(15, 3))
 counts.plot(kind='bar', ax=ax)
 ax.set_title('Frecuencia de padrones')
 ax.set_xlabel('Padrones')
