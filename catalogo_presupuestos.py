81a438b2a50d4382b155fd9ecb670edcbea8ebab
bFraire01 <mfrdobr@gmail.com>
2024-07-17 10:49:08 -0600

    Fusión de cambios

--- catalogo_presupuestos.py
+++ catalogo_presupuestos.py
@@ -5,43 +5,19 @@ import matplotlib.pyplot as plt
 #   Declarar el titulo de la aplicación
 st.set_page_config(page_title='Streamlit App', page_icon='📈', layout='wide')
 
-<<<<<<< HEAD
-#   Con la siguiente linea se pueden leer todas las hojas del archivo de Excel pero el tiempo de ejecución aumenta considerablemente
-#df = pd.read_excel('D:\Escritorio\Software\Streamlit\Analisis-de-Archivo.xlsx', sheet_name='Catalogo de Presupuestos')
-
-st.header("Catálogo de Presupuestos")
-df = pd.read_csv('D:\Escritorio\Software\Streamlit\Catalogo_presupuestos.csv')
-#   st.caption(r"A continuación se muestra la tabla \"Catálogo de presupuestos\"")
-#   st.dataframe(df)
-
-col1, col2, col3 = st.columns((2, 2, 2), gap='medium')
-with col1:
-   col1.metric("Trámites realizados",df.nombrePadron.size)
-with col2:
-   col2.metric("Tipos de padrones", len(df['nombrePadron'].dropna().str.upper().unique()))
-with col3:
-    col3.metric("Número de usuarios", len(df['user'].dropna().str.upper().unique()))
-=======
 # Con la siguiente linea se pueden leer todas las hojas del archivo de Excel 
 # pero el tiempo de ejecución aumenta considerablemente
-# df = pd.read_excel('D:\Escritorio\Software\Streamlit\Analisis-de-Archivo.xlsx', 
-# sheet_name='Catalogo de Presupuestos')
+# df = pd.read_excel('D:\Escritorio\Software\Streamlit\Analisis-de-Archivo.
+# xlsx', sheet_name='Catalogo de Presupuestos')
 
 
 df = pd.read_csv('Catalogo_presupuestos.csv')
 st.caption(r"A continuación se muestra la tabla \"Catálogo de presupuestos\"")
 st.dataframe(df)
->>>>>>> develop
 
 st.divider()
 
 st.header("Total (resultados filtrados)")
-<<<<<<< HEAD
-padron_selection = st.multiselect("Selecciona un padrón:", options=df['nombrePadron'].dropna().str.upper().unique(), placeholder="Escoge una opción")
-df_selection = df.query("nombrePadron == @padron_selection")
-total_sum = pd.to_numeric(df_selection['total'], errors='coerce').sum()
-st.caption(f"El total de los resultados obtenidos mediante el uso de los filtros es de:$ :blue[ _{total_sum}_]  MXN")
-=======
 
 # st.sidebar.header("Filtrar resultados")
 padron_selection = st.multiselect("Selecciona un padrón:", options=df['nombrePadron'].unique(), placeholder="Escoge una opción")
@@ -53,7 +29,6 @@ st.caption(f"El total de los resultados obtenidos mediante el uso de los filtros
 # Crear el gráfico de barras
 counts = df['nombrePadron'].value_counts()
 
->>>>>>> develop
 st.divider()
 
 st.header("Padrones")
@@ -61,13 +36,9 @@ st.caption(r"Se muestra una grafica de barras para facilitar la lectura de la fr
 st.bar_chart(df['nombrePadron'].str.capitalize().value_counts(), x_label="TRÁMITES", y_label="FRECUENCIA", height=600)
 st.divider()
 
-<<<<<<< HEAD
-#   Crear el gráfico de barras
-=======
 # Crear el gráfico de barras
 counts = df['marca'].value_counts()
 
->>>>>>> develop
 st.header("Frecuencia de Marcas de Autos")
 st.caption(r"Se muestra una grafica de barras para facilitar la lectura de la frecuencia de marcas de carros")
 
@@ -79,7 +50,7 @@ counts_marca = df['marca'].value_counts()
 #   Crear el gráfico de pastel para las marcas de autos
 fig, ax = plt.subplots(figsize=(12, 12))
 ax.pie(counts_marca, labels=counts_marca.index, autopct='%1.1f%%', startangle=90)
-ax.axis('equal')  # Equal aspect ratio asegura que el gráfico de pastel es circular
+ax.axis('equal')  # Equal aspect ratio asegura el gráfico de pastel  circular
 ax.set_title('Frecuencia de Marcas de Autos')
 #   Mostrar el gráfico de pastel en Streamlit
 st.pyplot(fig)
